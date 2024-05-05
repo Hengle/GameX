50c069d9e058cda7de881b73ed90ebeb38f8584a
Sky Morey <apphaus@outlook.com>
2024-05-05 10:10:38 -0500

    updates

---
+++ dotnet/Bethesda/GameX.Bethesda.Native/GameX.Bethesda.Native.cpp
@@ -0,0 +1,16 @@
+﻿// GameX.Bethesda.Native.cpp : Defines the entry point for the application.
+//
+
+#using <mscorlib.dll>
+#include "GameX.Bethesda.Native.h"
+
+//using namespace System;
+using namespace std;
+
+int main()
+{
+	//System::Console::WriteLine("Hello world");
+	//auto b = gcnew System::IO::MemoryStream();
+	cout << "Hello CMake." << endl;
+	return 0;
+}
