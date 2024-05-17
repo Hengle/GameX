6f16d22238d22d2e3290b6536d8c13ceed3975a9
Sky Morey <sky.morey@merkle.com>
2024-05-16 20:38:28 -0500

    updates

--- dotnet/Bullfrog/GameX.Bullfrog/Formats/PakBinary_Keeper.cs
+++ dotnet/Bullfrog/GameX.Bullfrog/Formats/PakBinary_Keeper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
@@ -16,51 +17,47 @@ namespace GameX.Bullfrog.Formats
             {
                 _ => Path.GetExtension(source.Path).ToLowerInvariant() switch
                 {
-                    ".dat" => (0, Binary_Dat.Factory),
+                    //".dat" => (0, Binary_Dat.Factory),
                     _ => (0, null),
                 }
             };
 
         #endregion
 
-        #region Header
+        const int TEXTURE_BLOCKS_STAT_COUNT_A = 544;  // Static textures in tmapa
+        const int TEXTURE_BLOCKS_STAT_COUNT_B = 544; // Static textures in tmapb
 
-        const uint MAGIC_SPR = 0x42465350;
-
-        [StructLayout(LayoutKind.Sequential, Pack = 0x1)]
-        struct SPR_Record
-        {
-            public uint Magic;
-            public uint Dummy1;
-            public uint FileCount;
-            public uint ExtraFiles;
-            public uint Dummy2;
-            public uint Dummy3;
-            public uint Dummy4;
-            public uint Dummy5;
-        }
-
-        #endregion
+        byte[] Data;
 
         public override Task Read(BinaryPakFile source, BinaryReader r, object tag)
         {
-            var magic = r.ReadUInt32();
+            const int TextureBlockSize = 32 * 32;
+            Data = Rnc.Unpack(r);
             var files = source.Files = new List<FileSource>();
-            switch (magic)
-            {
-                case MAGIC_SPR:
+            var fileName = Path.GetFileName(source.PakPath);
+            if (fileName.StartsWith("TMAPA"))
+                for (int i = 0, o = 0; i < TEXTURE_BLOCKS_STAT_COUNT_A; i++, o += TextureBlockSize)
+                    files.Add(new FileSource
                     {
-                        var count = r.ReadUInt32();
-                        var abc = r.ReadSArray<SPR_Record>((int)count);
-                        return Task.CompletedTask;
-                    }
-                default: throw new FormatException("BAD MAGIC");
-            }
+                        Path = $"texs/{i}.tex",
+                        Offset = o,
+                        FileSize = TextureBlockSize,
+                    });
+            else if (fileName.StartsWith("TMAPB"))
+                for (int i = 0, o = 0; i < TEXTURE_BLOCKS_STAT_COUNT_B; i++, o += TextureBlockSize)
+                    files.Add(new FileSource
+                    {
+                        Path = $"texs/{i}.tex",
+                        Offset = o,
+                        FileSize = TextureBlockSize,
+                    });
+            return Task.CompletedTask;
         }
 
         public override Task<Stream> ReadData(BinaryPakFile source, BinaryReader r, FileSource file, FileOption option = default)
         {
-            throw new NotImplementedException();
+            var bytes = Data.AsSpan((int)file.Offset, (int)file.FileSize);
+            return Task.FromResult((Stream)new MemoryStream(bytes.ToArray()));
         }
     }
 }
