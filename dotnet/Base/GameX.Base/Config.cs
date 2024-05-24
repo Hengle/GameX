392ca20d104f56102c6cace4c12a25315fd00049
Sky Morey <sky.morey@merkle.com>
2024-05-24 08:18:26 -0500

    updates

--- dotnet/Base/GameX.Base/Config.cs
+++ dotnet/Base/GameX.Base/Config.cs
@@ -149,14 +149,14 @@ namespace GameX
             ForcePath = "sample:0",
             Family = "Bullfrog",
             //Game = "P", // Populous
-            //Game = "P2", // Populous II: Trials of the Olympian Gods
+            Game = "P2", // Populous II: Trials of the Olympian Gods
             //Game = "S", // Syndicate
             //Game = "MC", // Magic Carpet
             //Game = "TP", // Theme Park
             //Game = "MC2", // Magic Carpet 2
             //Game = "S2", // Syndicate Wars
             //Game = "TH", // Theme Hospital
-            Game = "DK", // Dungeon Keeper
+            //Game = "DK", // Dungeon Keeper
             //Game = "P3", // Populous: The Beginning
             //Game = "DK2", // Dungeon Keeper 2
         };
