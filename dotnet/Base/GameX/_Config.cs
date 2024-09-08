9bf1788137312473ee2c78e307bed8267c012247
Sky Morey <apphaus@outlook.com>
2024-09-07 19:12:17 -0500

    updates

--- dotnet/Base/GameX/_Config.cs
+++ dotnet/Base/GameX/_Config.cs
@@ -68,7 +68,7 @@ namespace GameX
         public static GlobalOption Option = new()
         {
             ForceOpen = true,
-            ForcePath = "sample:0",
+            ForcePath = "sample:1",
             Family = "Bethesda",
             Game = "Morrowind", // The Elder Scrolls III: Morrowind
             //Game = "Oblivion", // The Elder Scrolls IV: Oblivion
